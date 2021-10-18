ad948aef115f4b7b22a4ba4167899e502ad20ec6
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2021-10-18 21:10:02 +0200

    website

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function(){ 
+    $(window).scroll(function () { 
+        var scroll = $(this).scrollTop();
+    if(scroll > 800){
+        $('.navbar-scroll').show();}
+        else{
+            $('.navbar-scroll').hide();
+        }
+    });
+
+})
+
+$(function(){ 
+    $(window).scroll(function () { 
+        var scroll = $(this).scrollTop();
+    if(scroll > 1500){
+        $('.navbar-scroll-pink').show();}
+        else{
+            $('.navbar-scroll-pink').hide();
+        }
+    });
+
+})
+$(function(){ 
+    $(window).scroll(function () { 
+        var scroll = $(this).scrollTop();
+    if(scroll > 2100){
+        $('.navbar-scroll-pp').show();}
+        else{
+            $('.navbar-scroll-pp').hide();
+        }
+    });
+
+})
+
+$(function(){ 
+    $(window).scroll(function () { 
+        var scroll = $(this).scrollTop();
+    if(scroll > 2800){
+        $('.navbar-scroll-y').show();}
+        else{
+            $('.navbar-scroll-y').hide();
+        }
+    });
+
+})
